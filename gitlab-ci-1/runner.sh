30fa0986e90ed258699d8e5aa1c02d3d433b7875
vld <vldspinninrecords@gmail.com>
2020-05-25 23:01:13 +0700

    #4 add logout button to profile page

--- gitlab-ci-1/runner.sh
+++ gitlab-ci-1/runner.sh
@@ -8,16 +8,7 @@ docker run -d --name $NAME --restart always -v /srv/$NAME/config:/etc/gitlab-run
 
 
 docker exec -it $NAME gitlab-runner register \
-  --run-untagged \
-  --locked=false \
-  --non-interactive \
-  --url ${GITLAB_CI_URL} \
-  --registration-token $TOKEN \
-  --description "docker-runner" \
-  --tag-list "linux,xenial,ubuntu,docker,vldops" \
-  --executor docker \
-  --docker-image "alpine:latest" \
-  --docker-privileged \
-  --docker-volumes "docker-certs-client:/certs/client" \
-  --env "DOCKER_DRIVER=overlay2" \
-  --env "DOCKER_TLS_CERTDIR=/certs"
+  --run-untagged --locked=false --non-interactive --url ${GITLAB_CI_URL} --registration-token $TOKEN \
+  --description "script-runner" --tag-list "linux,xenial,ubuntu,docker,vldops" --executor docker \
+  --docker-image "alpine:latest" --docker-privileged --docker-volumes "docker-certs-client:/certs/client" \
+  --env "DOCKER_DRIVER=overlay2" --env "DOCKER_TLS_CERTDIR=/certs"
